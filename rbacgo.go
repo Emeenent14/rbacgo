1b01bf6b8ac201d478c4084d77d835a9996d16eb
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2026-06-17 23:40:00 +0100

    functionality set and remove parents

--- rbacgo.go
+++ rbacgo.go
@@ -4,6 +4,10 @@ import (
 	"errors"
 )
 
+var (
+	empty = struct{}{}
+)
+
 type Roles[T comparable] map[T]*Role[T]
 type Set[T comparable] map[T]struct{}
 
@@ -45,3 +49,30 @@ func (rbac *RBAC[T]) ListRoles() []T{
 	}
 	return roleSlice
 }
+
+//add parent 
+func (rbac *RBAC[T]) SetParent(parentId, childId T)error{
+	if _, exists := rbac.roles[parentId];!exists{
+		return errors.New("The parent must be an existing role")
+	}
+	if _, exists := rbac.roles[childId];!exists{
+		return errors.New("The child must be an existing role")
+	}
+	if _, ok := rbac.parents[childId];!ok{
+		rbac.parents[childId] = make(Set[T])
+	}
+	rbac.parents[childId][parentId] = empty
+	return nil
+}
+
+//Remove parent
+func (rbac *RBAC[T]) RemoveParent(parentId, childId T)error{
+	if _,exists := rbac.roles[childId]; !exists{
+		return errors.New("The given child role does not exist")
+	}
+	if _,exists := rbac.roles[parentId]; !exists{
+		return errors.New("The given parent role does not exist")
+	}
+	delete(rbac.parents[childId], parentId)
+	return nil
+} 
