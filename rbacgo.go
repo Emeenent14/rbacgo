256714155e109c4935cd20dd01365a26f26b9a96
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2026-06-17 22:39:55 +0100

    role add, revoke, and list reciever functions on rbac

--- rbacgo.go
+++ rbacgo.go
@@ -4,39 +4,33 @@ import "fmt"
 
 type Roles[T comparable] map[T]*Role[T]
 
-type rbacgo struct {
-	roles Roles[T]
+type rbacgo[T comparable] struct {
+	roles   Roles[T]
 	parents map[T]map[T]struct{}
 }
 
-func NewRbac(role *Role[T]) Roles[T]{
-	return &rbacgo{
-		roles: make(map[T]*Role[T]),
+func NewRbac[T comparable]() *rbacgo[T] {
+	return &rbacgo[T]{
+		roles:   make(map[T]*Role[T]),
 		parents: make(map[T]map[T]struct{}),
 	}
 }
 
 //Add role
-func (rbac *rbacgo) AddRole(role *Role[T]){
+func (rbac *rbacgo[T]) AddRole(role *Role[T]){
 	rbac.roles[role.roleId] = role	
 }
 
 //Remove role
-func (rbac *rbacgo) RemoveRole(roleId T){
+func (rbac *rbacgo[T]) RemoveRole(roleId T){
 	delete(rbac.roles, roleId)
 }		
 
 //List roles
-func (rbac *rbacgo) ListRoles() []T{
+func (rbac *rbacgo[T]) ListRoles() []T{
 	roleList := make([]T, 0, len(rbac.roles))	
 	for roleId := range rbac.roles{
-		append(roleList, roleId)
+		roleList = append(roleList, roleId)
 	}	
 	return roleList
 }
-
-
-
-
-
-
