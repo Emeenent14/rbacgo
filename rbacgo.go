ab7473eec1aaab0186ca02d78c1e7e4a2e932cad
Emeenent14 <chukwuemekaoguejiofor14@gmail.com>
2026-06-17 23:15:00 +0100

    Added role inheritance to the RBAC registry

--- rbacgo.go
+++ rbacgo.go
@@ -5,21 +5,22 @@ import (
 )
 
 type Roles[T comparable] map[T]*Role[T]
+type Set[T comparable] map[T]struct{}
 
-type rbacgo[T comparable] struct {
-	roles   Roles[T]
-	parents map[T]map[T]struct{}
+type RBAC[T comparable] struct {
+	roles Roles[T]
+	parents map[T]Set[T]
 }
 
-func NewRbac[T comparable]() *rbacgo[T] {
-	return &rbacgo[T]{
-		roles:   make(map[T]*Role[T]),
-		parents: make(map[T]map[T]struct{}),
+func NewRBAC[T comparable]() *RBAC[T] {
+	return &RBAC[T]{
+		roles: make(Roles[T]),
+		parents: make(map[T]Set[T]),
 	}
 }
 
 //Add role
-func (rbac *rbacgo[T]) AddRole(role *Role[T]) error {
+func (rbac *RBAC[T]) AddRole(role *Role[T]) error {
 	if _, exists := rbac.roles[role.roleId]; !exists {
 		rbac.roles[role.roleId] = role
 		return nil
@@ -27,20 +28,20 @@ func (rbac *rbacgo[T]) AddRole(role *Role[T]) error {
 	return errors.New("This role already exists")
 }
 
-//Remove role
-func (rbac *rbacgo[T]) RemoveRole(roleId T) error {
-	if _, exists := rbac.roles[roleId]; !exists {
+//Revoke role
+func (rbac *RBAC[T]) RevokeRole(roleId T)error{
+	if _, exists := rbac.roles[roleId]; !exists{
 		return errors.New("The role does not exist")
 	}
 	delete(rbac.roles, roleId)
 	return nil
-}		
+}
 
-//List roles
-func (rbac *rbacgo[T]) ListRoles() []T{
-	roleList := make([]T, 0, len(rbac.roles))	
-	for roleId := range rbac.roles{
-		roleList = append(roleList, roleId)
-	}	
-	return roleList
+//List all role 
+func (rbac *RBAC[T]) ListRoles() []T{
+	roleSlice := make([]T, 0, len(rbac.roles))
+	for role := range rbac.roles{
+		roleSlice = append(roleSlice, role)
+	}
+	return roleSlice
 }
